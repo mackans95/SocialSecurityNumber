0a49d8544b1cec89fb5e9bdabbba28d94d1453f0
Marcus Sarnes <mackan.g12@gmail.com>
2020-09-11 12:42:47 +0200

    Added CalculateGereration() and made GetGender an enum

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ namespace SocialSecurityNumber
             string socialSecurityNumber;
             string firstName;
             string lastName;
-            string generation;
 
             if (args.Length > 0)
             {
@@ -35,6 +34,59 @@ namespace SocialSecurityNumber
 
             var gender = GetGender(socialSecurityNumber);
 
+            socialSecurityNumber = ValidSocialSecurityNumber(socialSecurityNumber);
+
+            var birthDate = DateTime.ParseExact(socialSecurityNumber.Substring(0, 8), "yyyyMMdd", CultureInfo.InvariantCulture);
+
+            var age = CalculateAge(socialSecurityNumber);
+
+            var generation = CalculateGeneration(birthDate);
+
+            // -25 in the rows below indicates the amount of spaces to print before the other variables
+            Console.Clear();
+            Console.WriteLine($"{"Name: ",-25}{firstName} {lastName}");
+            Console.WriteLine($"{"Social Security Number: ",-25}{socialSecurityNumber}");
+            Console.WriteLine($"{"Gender: ",-25}{gender}");
+            Console.WriteLine($"{"Age: ",-25}{age}");
+            Console.WriteLine($"{"Generation: ",-25}{generation}");
+            Console.ReadKey();
+        }
+
+        private static string CalculateGeneration(DateTime birthDate)
+        {
+            string generation;
+            const int silentGenCutoff = 1945;
+            const int babyBoomerCutoff = 1964;
+            const int generationXCutoff = 1980;
+            const int millenialCutoff = 1996;
+            const int generationZCutoff = 2012;
+
+            if (birthDate.Year >= 1977 && birthDate.Year <= 1983)
+            {
+                generation = "Xennial";
+            }
+            else
+            {
+                generation = (birthDate.Year <= silentGenCutoff ? "isSilentGen" :
+                        birthDate.Year <= babyBoomerCutoff ? "isBabyBoomer" :
+                        birthDate.Year <= generationXCutoff ? "isGenX" :
+                        birthDate.Year <= millenialCutoff ? "isMillenial" :
+                        birthDate.Year <= generationZCutoff ? "isGenZ" : default) switch
+                    {
+                        "isSilentGen" => "Silent Generation",
+                        "isBabyBoomer" => "Baby Boomer",
+                        "isGenX" => "Generation X",
+                        "isMillenial" => "Millenial",
+                        "isGenZ" => "Generation Z",
+                        _ => "Generation Alpha",
+                    };
+            }
+
+            return generation;
+        }
+
+        private static string ValidSocialSecurityNumber(string socialSecurityNumber)
+        {
             //This regex below will now make sure only SSN:s that match it (8 digits with or without 
             //a hyphen, then the last 4) will work, all else is invalid
             var ssnFormat = new Regex(@"^\d{8}[-\s]{0,1}\d{4}$");
@@ -51,44 +103,7 @@ namespace SocialSecurityNumber
                 }
             }
 
-            var birthDate = DateTime.ParseExact(socialSecurityNumber.Substring(0, 8), "yyyyMMdd", CultureInfo.InvariantCulture);
-
-            var age = CalculateAge(socialSecurityNumber);
-
-            const int silentGenCutoff = 1945;
-            const int babyBoomerCutoff = 1964;
-            const int generationXCutoff = 1980;
-            const int millenialCutoff = 1996;
-            const int generationZCutoff = 2012;
-
-            if (birthDate.Year >= 1977 && birthDate.Year <= 1983)
-            {
-                generation = "Xennial";
-            }
-            else
-            {
-                generation = (birthDate.Year <= silentGenCutoff ? "isSilentGen" :
-                              birthDate.Year <= babyBoomerCutoff ? "isBabyBoomer" :
-                              birthDate.Year <= generationXCutoff ? "isGenX" :
-                              birthDate.Year <= millenialCutoff ? "isMillenial" :
-                              birthDate.Year <= generationZCutoff ? "isGenZ" : default) switch
-                {
-                    "isSilentGen" => "Silent Generation",
-                    "isBabyBoomer" => "Baby Boomer",
-                    "isGenX" => "Generation X",
-                    "isMillenial" => "Millenial",
-                    "isGenZ" => "Generation Z",
-                    _ => "Generation Alpha",
-                };
-            }
-            // -25 in the rows below indicates the amount of spaces to print before the other variables
-            Console.Clear();
-            Console.WriteLine($"{"Name: ",-25}{firstName} {lastName}");
-            Console.WriteLine($"{"Social Security Number: ",-25}{socialSecurityNumber}");
-            Console.WriteLine($"{"Gender: ",-25}{gender}");
-            Console.WriteLine($"{"Age: ",-25}{age}");
-            Console.WriteLine($"{"Generation: ",-25}{generation}");
-            Console.ReadKey();
+            return socialSecurityNumber;
         }
 
         private static int CalculateAge(string socialSecurityNumber)
@@ -106,15 +121,21 @@ namespace SocialSecurityNumber
             return age;
         }
 
-        private static string GetGender(string socialSecurityNumber)
+        private static Gender GetGender(string socialSecurityNumber)
         {
             var genderNumber = int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 2, 1));
 
             var isFemale = genderNumber % 2 == 0;
             
-            var gender = isFemale ? "Female" : "Male";
+            var gender = isFemale ? Gender.Female : Gender.Male;
             
             return gender;
+        }
+
+        private enum Gender
+        {
+            Female,
+            Male
         }
     }
 }
